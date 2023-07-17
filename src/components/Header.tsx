6ced284471050ad951c90098bc65f6dec98a647f
LizzColDev <lizzcoldev@gmail.com>
2023-07-17 17:38:16 -0500

    Added styles and color palette to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,22 @@
 import { Link } from 'react-router-dom'
 import { Navbar, Nav } from 'react-bootstrap'
-
+import './Header.css'
 function Header () {
   return (
-    <Navbar bg='light' expand='lg'>
-      <Navbar.Brand as={Link} to='/'>My Portfolio</Navbar.Brand>
-      <Navbar.Toggle aria-controls='basic-navbar-nav' />
-      <Navbar.Collapse id='basic-navbar-nav'>
-        <Nav className='mr-auto'>
-          <Nav.Link as={Link} to='/'>Home</Nav.Link>
-          <Nav.Link as={Link} to='/projects'>Projects</Nav.Link>
-          <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <header>
+      <Navbar expand='md' className='mx-auto'>
+        <Navbar.Brand as={Link} to='/'>My Portfolio</Navbar.Brand>
+        <Navbar.Toggle className='custom-toggle' aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
+          <Nav className='mr-auto'>
+            <Nav.Link as={Link} to='/'>Home</Nav.Link>
+            <Nav.Link as={Link} to='/projects'>Projects</Nav.Link>
+            <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+
   )
 }
 
